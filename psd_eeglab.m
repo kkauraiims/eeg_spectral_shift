6667c386b8ab443513d6b612d135eb305f4ecb6c
Kirandeep Kaur <89849204+kkauraiims@users.noreply.github.com>
2026-05-29 15:38:06 +0100

    Update and rename PSD_EEGLAB.m to psd_eeglab.m

--- psd_eeglab.m
+++ psd_eeglab.m
@@ -1,21 +1,32 @@
-%% Load subject file 
+%% Compute EEG band power using EEGLAB spectopo
+% This script loads an EEGLAB .set file, computes channel-level spectra using
+% EEGLAB's spectopo function, and estimates mean power in canonical frequency
+% bands [delta, theta, alpha, beta, gamma (30-80Hz)]
+%
+% Part of the frequency-band extraction workflow is based on standard
+% EEGLAB spectopo examples and EEGLAB mailing-list guidance.
+%
+% References:
+%   EEGLAB spectopo documentation:
+%   https://sccn.ucsd.edu/~arno/eeglab/auto/spectopo.html
+%
+%   EEGLAB mailing list example on computing frequency-band power:
+%   https://sccn.ucsd.edu/pipermail/eeglablist/2015/008910.html
 
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab; % open eeglab
 
 %load dataset
-EEG = pop_loadset('filename','eeglab_data.set','filepath','C:\\Users\\KIRAN\\Desktop\\eeglab_current\\eeglab14_1_2b\\sample_data\\');
+EEG = pop_loadset('filename','eeglab_data.set','filepath','\path\to\data');
 
 [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0); %store dataset in ALLEEG
 
 EEG = eeg_checkset( EEG ); % check the fields of EEG data set 
 
 %% Plot EEG spectra and retrieve corresponding frequencies
-
 % EEG.data = EEG chan x time-points matrix
 % EEG.srate = sampling rate of EEG 
 % spectra = power spectra (mean power over epochs), in dB
 % freqs =frequencies of spectra (Hz)
-
 
 [spectra,freqs] = spectopo(EEG.data(1:128,:,:), 0, EEG.srate); % no. of data frames = data length 
 %% Set the following frequency bands: delta=1-4, theta=4-8, alpha=8-12, beta=13-30, gamma=30-80
@@ -26,24 +37,21 @@ alphaIdx = find(freqs>8 & freqs<13);
 betaIdx  = find(freqs>13 & freqs<30);
 gammaIdx = find(freqs>30 & freqs<80);
 
-%% Compute absolute power
- deltaPower = mean(10.^(spectra(deltaIdx)/10));
- thetaPower = mean(10.^(spectra(thetaIdx)/10));
- alphaPower = mean(10.^(spectra(alphaIdx)/10));
- betaPower  = mean(10.^(spectra(betaIdx)/10));
- gammaPower = mean(10.^(spectra(gammaIdx)/10));
+% Compute absolute power
+deltaPower = mean(10.^(spectra(deltaIdx)/10));
+thetaPower = mean(10.^(spectra(thetaIdx)/10));
+alphaPower = mean(10.^(spectra(alphaIdx)/10));
+betaPower  = mean(10.^(spectra(betaIdx)/10));
+gammaPower = mean(10.^(spectra(gammaIdx)/10));
 
-
-%% %% Compute power in dB
-
+% Compute power in dB
 deltaPower = mean(spectra(deltaIdx));
 thetaPower = mean(spectra(thetaIdx));
 alphaPower = mean(spectra(alphaIdx));
 betaPower  = mean(spectra(betaIdx));
 gammaPower = mean(spectra(gammaIdx));
 
-%% Export results in excel sheet
-
+% Export results in excel sheet
 % create an array of power in bands
 power_in_bands = [deltaPower thetaPower alphaPower betaPower gammaPower];
 
